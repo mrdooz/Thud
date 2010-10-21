163facf34a6a5851f3bc446ee2db131c81396e6c
Magnus Osterlind <magnus.osterlind@gmail.com>
2010-10-21 07:17:56 +0200

    stuff

--- thud.cpp
+++ thud.cpp
@@ -9,18 +9,10 @@
 #include <celsus/effect_wrapper.hpp>
 #include <D3DX10math.h>
 
-// 2d renderer
-struct Vec2
-{
-  Vec2(float x, float y) : x(x), y(0) {}
-  Vec2() {}
-  union {
-    struct {float x;  float y; };
-    float d[2];
-  };
-};
+const float kPi = (float)D3DX_PI;
 
 char shader[] = " "\
+"float4 scale; 											  "\
 "struct psInput												"\
 "{																		"\
 "	float4 pos : SV_Position;						"\
@@ -32,11 +24,11 @@ char shader[] = " "\
 "	float4 pos : SV_Position;						"\
 "	float4 col : Color;									"\
 "};																		"\
-"																			"\
 "psInput vsMain(in vsInput v)					"\
 "{																		"\
 "	psInput o = (psInput)0;							"\
-"	o.pos = v.pos;											"\
+"	o.pos.x = v.pos.x * scale.x;			  "\
+"	o.pos.y = v.pos.y * scale.y;			  "\
 "	o.col = v.col;											"\
 "	return o;														"\
 "}																		"\
@@ -46,6 +38,7 @@ char shader[] = " "\
 "	return v.col;"\
 "}";
 
+// Thud - 2d renderer
 struct Thud
 {
 	Thud();
@@ -60,9 +53,7 @@ struct Thud
 
 	void add_canvas();
 
-  // (0,0) is top-left
-  void set_viewport(const Vec2& extents);
-  void set_viewport(const Vec2& top_left, const Vec2& extents);
+  void set_extents(const D3DXVECTOR2& extents);
 
   void set_fill(const D3DXCOLOR& col);
   void set_stroke(const D3DXCOLOR& col);
@@ -70,10 +61,10 @@ struct Thud
   void clear(const D3DXCOLOR& col);
 
   void set_circle_segments(int num_segments);
-  void circle(const Vec2& o, float r);
-  void circle(const Vec2& o, float r, int segments);
+  void circle(const D3DXVECTOR3& o, float r);
+  void circle(const D3DXVECTOR3& o, float r, int segments);
 
-  void rect(const Vec2& top_left, const Vec2& size);
+  void rect(const D3DXVECTOR3& top_left, const D3DXVECTOR3& size);
 
   void start_frame();
   void render();
@@ -94,27 +85,41 @@ struct Thud
 	struct Canvas
 	{
 		Canvas()
-			: extents(Vec2(2,2))
-			, top_left(-1,1)
+			: extents(2,2)
+      , scale(1,1)
+      , ptr(nullptr)
 		{
 		}
 
 		bool init()
 		{
-			RETURN_ON_FAIL_BOOL_E(_verts.create(32 * 1024));
+			RETURN_ON_FAIL_BOOL_E(verts.create(32 * 1024));
 			return true;
 		}
 
-		Vec2 extents;
-		Vec2 top_left;
-		DynamicVb<PosCol> _verts;
+    void map()
+    {
+      ptr = verts.map();
+    }
+
+    int unmap()
+    {
+      const int c = verts.unmap(ptr);
+      ptr = nullptr;
+      return c;
+    }
+
+    D3DXVECTOR2 scale;
+		D3DXVECTOR2 extents;
+		DynamicVb<PosCol> verts;
+    PosCol *ptr;
 	};
 
 	EffectWrapper *_effect;
 	CComPtr<ID3D11InputLayout> _layout;
-
-  std::deque<State> _states;
-	std::deque<Canvas> _canvases;
+  CComPtr<ID3D11Buffer> _cbuffer;
+  std::deque<State> _state_stack;
+	std::deque<Canvas> _canvas_stack;
   static Thud *_instance;
 };
 
@@ -134,9 +139,9 @@ Thud& Thud::instance()
 bool Thud::init()
 {
   // default state
-  _states.push_back(State());
-	_canvases.push_back(Canvas());
-	RETURN_ON_FAIL_BOOL_E(_canvases.back().init());
+  _state_stack.push_back(State());
+	_canvas_stack.push_back(Canvas());
+	RETURN_ON_FAIL_BOOL_E(_canvas_stack.back().init());
 	
 	_effect = new EffectWrapper();
 	RETURN_ON_FAIL_BOOL_E(_effect->load_shaders(shader, sizeof(shader), "vsMain", NULL, "psMain"));
@@ -145,6 +150,8 @@ bool Thud::init()
 		add("SV_POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0).
 		add("COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0).
 		create(_layout, _effect));
+
+  RETURN_ON_FAIL_BOOL_E(create_cbuffer(Graphics::instance().device(), 1 * sizeof(D3DXVECTOR4), &_cbuffer.p));
 
   return true;
 }
@@ -158,26 +165,22 @@ bool Thud::close()
   return true;
 }
 
-void Thud::set_viewport(const Vec2& extents)
+void Thud::set_extents(const D3DXVECTOR2& extents)
 {
-	_canvases.back().top_left = Vec2(0,0);
-  _canvases.back().extents = extents;
-}
-
-void Thud::set_viewport(const Vec2& top_left, const Vec2& extents)
-{
-  _canvases.back().top_left = top_left;
-  _canvases.back().extents = extents;
+  Canvas& cur = _canvas_stack.back();
+  cur.extents = extents;
+  cur.scale.x = 2 / extents.x;
+  cur.scale.y = 2 / extents.y;
 }
 
 void Thud::set_fill(const D3DXCOLOR& col)
 {
-  _states.back().fill = col;
+  _state_stack.back().fill = col;
 }
 
 void Thud::set_stroke(const D3DXCOLOR& col)
 {
-  _states.back().stroke = col;
+  _state_stack.back().stroke = col;
 }
 
 void Thud::clear(const D3DXCOLOR& col)
@@ -187,32 +190,71 @@ void Thud::clear(const D3DXCOLOR& col)
 
 void Thud::start_frame()
 {
+  Canvas& canvas = _canvas_stack.back();
+  canvas.map();
 
+  Graphics& graphics = Graphics::instance();
+  ID3D11Device* device = graphics.device();
+  ID3D11DeviceContext* context = graphics.context();
+
+  // set the cbuffer
+  D3DXVECTOR4 *scale = (D3DXVECTOR4 *)map_buffer(context, _cbuffer);
+  scale->x = canvas.scale.x;
+  scale->y = canvas.scale.y;
+  unmap_buffer(context, _cbuffer);
+  context->VSSetConstantBuffers(0, 1, &_cbuffer.p);
 }
 
 void Thud::render()
 {
+  Graphics& graphics = Graphics::instance();
+  ID3D11Device* device = graphics.device();
+  ID3D11DeviceContext* context = graphics.context();
 
+  Canvas& canvas = _canvas_stack.back();
+  const int num_verts = canvas.unmap();
+
+  context->OMSetDepthStencilState(graphics.default_dss(), graphics.default_stencil_ref());
+  context->OMSetBlendState(graphics.default_blend_state(), graphics.default_blend_factors(), graphics.default_sample_mask());
+
+  _effect->set_shaders(context);
+  context->IASetInputLayout(_layout);
+  set_vb(context, canvas.verts.vb(), canvas.verts.stride);
+  context->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+  context->Draw(num_verts, 0);
 }
 
-void Thud::circle(const Vec2& o, float r)
+void Thud::circle(const D3DXVECTOR3& o, float r)
 {
-
+  const State& state = _state_stack.back();
+  Canvas& canvas = _canvas_stack.back();
+  PosCol*& ptr = canvas.ptr;
+  const float inc = 2 * kPi / (state.circle_segments);
+  float ofs = 0;
+  D3DXVECTOR3 cur = D3DXVECTOR3(o.x + r * cosf(ofs), o.y + r * sinf(ofs), o.z);
+  for (int i = 0; i < state.circle_segments; ++i) {
+    D3DXVECTOR3 next = D3DXVECTOR3(o.x + r * cosf(ofs + inc), o.y + r * sinf(ofs + inc), o.z);
+    *ptr++ = PosCol(o, state.fill);
+    *ptr++ = PosCol(next, state.fill);
+    *ptr++ = PosCol(cur, state.fill);
+    cur = next;
+    ofs += inc;
+  }
 }
 
-void Thud::rect(const Vec2& top_left, const Vec2& size)
+void Thud::rect(const D3DXVECTOR3& top_left, const D3DXVECTOR3& size)
 {
 
 }
 
 void Thud::push_state()
 {
-  _states.push_back(State());
+  _state_stack.push_back(State());
 }
 
 void Thud::pop_state()
 {
-  _states.pop_back();   
+  _state_stack.pop_back();   
 }
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
@@ -271,7 +313,7 @@ int WINAPI WinMain( __in HINSTANCE hInstance, __in_opt HINSTANCE hPrevInstance, 
   if (!thud.init())
     return 1;
 
-  thud.set_viewport(Vec2(-1, 1), Vec2(2, 2));
+  thud.set_extents(D3DXVECTOR2(10, 10));
 
   MSG msg;
   ZeroMemory(&msg, sizeof(msg));
@@ -280,12 +322,15 @@ int WINAPI WinMain( __in HINSTANCE hInstance, __in_opt HINSTANCE hPrevInstance, 
       TranslateMessage(&msg);
       DispatchMessage(&msg);
     } else {
+      graphics.clear(D3DXCOLOR(0.5f, 0.5f, 0.5f, 1));
+      thud.start_frame();
       thud.clear(D3DXCOLOR(0, 1, 0, 1));
-      thud.circle(Vec2(0,0), 0.5f);
+      thud.set_fill(D3DXCOLOR(1,1,1,1));
+      thud.circle(D3DXVECTOR3(0,0,0.5f), 0.015f);
+      thud.render();
       graphics.present();
     }
   }
-
 
   thud.close();
   graphics.close();
